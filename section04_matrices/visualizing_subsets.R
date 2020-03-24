e9587928ca71c1bd9e9414804b4aea45f2bda97b
Rusty Mann <mannrusty@gmail.com>
2020-03-23 21:59:08 -0700

    functions and visualizationg subsets

---
+++ section04_matrices/visualizing_subsets.R
@@ -0,0 +1,20 @@
+
+
+
+
+Data <- MinutesPlayed[1:3,]
+matplot(t(Data), type="b", pch=15:18, col=c(1:4, 6))
+legend("bottomleft", inset=0.01, legend=Players[1:3], col=c(1:4, 6), pch=15:18, horiz=F)
+
+
+Data
+
+#Just Kobe; use drop to make 1-D vector a matrix
+Data <- MinutesPlayed[1,,drop=F]
+matplot(t(Data), type="b", pch=15:18, col=c(1:4, 6))
+legend("bottomleft", inset=0.01, legend=Players[1], col=c(1:4, 6), pch=15:18, horiz=F)
+
+
+
+
+
